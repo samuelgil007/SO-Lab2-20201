4ac5848a56df8f5496158f26d8b57df155636a82
“samuelgil007” <sagilar15@gmail.com>
2020-11-14 14:44:32 -0500

    Funciona hasta redireccion, test 8

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -3,10 +3,13 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+
 char directorio[30];
 int salidaGlobal, numPath, numPath = 2;
 char **paths;
 char error_message[30] = "An error has occurred\n";
+int redirection;
 
 void type_prompt()
 {
@@ -34,19 +37,20 @@ int estaEnElPath(char command[])
         if (paths[i] != NULL)
         {
 
-            char esta[30];            
+            char esta[30];
             strcpy(esta, paths[i]);
             strcat(esta, "/");
             strcat(esta, command);
+
             if (access(esta, X_OK) == 0)
             {
                 return i;
             }
 
             // Si no se encuentra directorio no se ejecuta el comando
-            if (i == (numPath - 1))
+            if (i == (numPath - 1) || strcmp(command, "\n") == 0 || command == NULL)
             {
-                /* printf("No se puede encontrar ese comando en el path: %s\n", command); */               
+                /* printf("No se puede encontrar ese comando en el path: %s\n", command); */
                 return 99;
             }
         }
@@ -54,21 +58,35 @@ int estaEnElPath(char command[])
     return 99;
 }
 
-void leer_comando(char cmd[], char *par[], char line[])
+int leer_comando(char cmd[], char *par[], char line[])
 {
     int i = 0;
+    int positionRedi = -1;
     char *array[100];
-    strtok(line, "\n");
+    strtok(line, "\t\a\n\r");
 
     // Extract the first token
-    char *token = strtok(line, " \r");
+    char *token = strtok(line, " \t\a\n\r");
     // loop through the string to extract all other tokens
 
     while (token != NULL)
     {
-
+        //controlo que si hay un > se sepa.
+        if (strcmp(token, ">") == 0)
+        {
+            if (redirection == 0 && i != 0)
+            {
+                redirection = 1;
+                positionRedi = i;
+            }
+            else
+            {
+                redirection = -1;
+                //positionRedi= -1;
+            }
+        }
         array[i++] = token;
-        token = strtok(NULL, " ");
+        token = strtok(NULL, " \t\a\n\r");
     }
 
     // La primera palabra es el comando
@@ -84,81 +102,157 @@ void leer_comando(char cmd[], char *par[], char line[])
         par[j] = array[j];
     }
     par[i] = NULL; //Terminar la lista de parametros
+
+    //pregunto por el caso favorable (para ver luego si si es favorable en verdad)
+    if (redirection == 1 && positionRedi != -1)
+    {
+        if ((i - (positionRedi + 1)) != 1)
+        {
+            //quiere decir que hay 2 o mas parametros ala derecha de > , se deja la
+            //posicion del primer archivo para guardar alli el mensaje de error
+            redirection = -1;
+        }
+        if (array[positionRedi + 1] == NULL)
+        {
+            //como cumplio que desps del > hay nada se pone como malo
+            redirection = -1;
+            positionRedi = -1;
+            write(STDERR_FILENO, error_message, strlen(error_message));
+            //exit(1);
+        }
+    }
+
+    return positionRedi;
 }
 
-void ejecutar_comando(char command[], char *parameters[], char line[])
+void ejecutar_comando(char command[], char *parameters[], char line[], int posRedi)
 {
     //identifica comandos, si no lo encuentra hace un bin por defecto
     char cmd[100];
-    int esBin = estaEnElPath(command);
-    if (strcmp(command, "exit") == 0)
+    if (command != NULL)
     {
-        if (parameters[1] != NULL)
+        int esBin = estaEnElPath(command);
+        if (strcmp(command, "exit") == 0)
         {
-            write(STDERR_FILENO, error_message, strlen(error_message));
+            if (parameters[1] != NULL)
+            {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+            }
+            else
+            {
+                salidaGlobal = 1;
+                return;
+            }
         }
-        else
+        else if (strcmp(command, "path") == 0)
         {
-            salidaGlobal = 1;
+
+            if (numPath == 0)
+            {
+                char **newPaths = malloc((1) * sizeof(char *));
+                newPaths[0] = "l";
+                paths = newPaths;
+            }
+            else
+            {
+                char **newPaths = malloc((numPath) * sizeof(char *));
+                for (int i = 0; i < numPath; i++)
+                {
+                    newPaths[i] = parameters[i];
+                }
+                paths = newPaths;
+            }
             return;
         }
-    }
-    else if (strcmp(command, "path") == 0)
-    {
-
-        if (numPath == 0)
+        else if (strcmp(command, "cd") == 0)
         {
-            char **newPaths = malloc((1) * sizeof(char *));
-            newPaths[0] = "l";
-            paths = newPaths;
-        }
-        else
-        {
-            char **newPaths = malloc((numPath) * sizeof(char *));
-            for (int i = 0; i < numPath; i++)
+            if (chdir(parameters[1]) != 0 && parameters[1] != NULL)
             {
-                newPaths[i] = parameters[i];
+                /* printf("llega aqui \n");
+            printf("No se puede encontrar ese directorio %s\n", parameters[1]); */
+                write(STDERR_FILENO, error_message, strlen(error_message));
             }
-            paths = newPaths;
+            else if (parameters[1] == NULL)
+            {
+                /*  printf("Error 0 o mas de 1 argumento \n"); */
+                write(STDERR_FILENO, error_message, strlen(error_message));
+            }
+            else
+            {
+                /* printf("ingresó a %s \n",parameters[1]); //menos .. */
+                //chdir(parameters[1]); vuela
+                strcat(directorio, parameters[1]);
+            }
+            return;
         }
-        return;
-    }
-    else if (strcmp(command, "cd") == 0)
-    {
-        if (chdir(parameters[1]) != 0 && parameters[1] != NULL)
+        else if (esBin != 99)
         {
-            /* printf("No se puede encontrar ese directorio %s\n", error_message); */
-            write(STDERR_FILENO, error_message, strlen(error_message));
-        }
-        else if (parameters[1] == NULL)
-        {
-            /* printf("Error 0 o mas de 1 argumento \n"); */
-            write(STDERR_FILENO, error_message, strlen(error_message));
+            if (fork() != 0)
+            {
+                wait(NULL);
+            }
+            else
+            {
+                if (redirection == 0)
+                {
+                    strcpy(cmd, paths[esBin]);
+                    strcat(cmd, "/");
+                    strcat(cmd, command);
+                    execv(cmd, parameters); // ejecutar comando
+                    write(STDERR_FILENO, error_message, strlen(error_message));
+                    exit(1);
+                }
+                else
+                {
+                    char *finalParams[posRedi];
+                    char *documento = parameters[posRedi + 1];
+                    //logica crear el archivo y poner a escribir en el
+                    char outputFile[32];
+                    sscanf(documento, "%s", outputFile);
+                    int fd = creat(outputFile, 0644); // create new file
+                    int newfd = dup2(fd, 1);          // redirects stdout to outputFile
+                    if (newfd == -1)
+                    {
+                        perror("dup2");
+                        exit(1);
+                    }
+                    //pregunto si hay un comando bueno
+                    if (redirection == 1)
+                    {
+                        for (int i = 0; i < posRedi; i++)
+                        {
+                            finalParams[i] = parameters[i];
+                            //printf("%s \n", finalParams[i]);
+                        }
+                        finalParams[posRedi] = NULL;
+                        //printf("el documento se llama: %s \n", documento);
+                    }
+                    strcpy(cmd, paths[esBin]);
+                    strcat(cmd, "/");
+                    strcat(cmd, command);
+                    //ejecuto el archivo o error
+                    if (redirection == 1 && posRedi != -1)
+                    {
+                        int pepe = execv(cmd, finalParams);
+                        if (pepe == -1)
+                        {
+                            write(STDERR_FILENO, error_message, strlen(error_message));
+                        }
+                        printf("rip\n");
+                        exit(1);
+                    }
+                    else if (redirection == -1)
+                    {
+                        printf("An error has occurred");
+                        exit(1);
+                    }
+                }
+            }
         }
         else
         {
-            /* printf("ingresó a %s \n",parameters[1]); //menos .. */
-            //chdir(parameters[1]); vuela
-            strcat(directorio, parameters[1]);
+            write(STDERR_FILENO, error_message, strlen(error_message));
         }
-        return;
-    }
-    else if (esBin != 99)
-    {
-        if (fork() != 0)
-        {
-            wait(NULL);
-        }
-        else
-        {
-
-            strcpy(cmd, paths[esBin]);
-            strcat(cmd, "/");
-            strcat(cmd, command);
-            execv(cmd, parameters); // ejecutar comando
-        }
-    }else {
-        write(STDERR_FILENO, error_message, strlen(error_message));
     }
 }
 
@@ -185,6 +279,7 @@ int main(int argc, char *argv[])
         {
             int linex = 0;
             salidaGlobal = 0;
+            redirection = 0;
             while (salidaGlobal != 1)
             {
                 char *line = NULL;
@@ -203,28 +298,35 @@ int main(int argc, char *argv[])
                 {
                     salidaGlobal = 1;
                 }
+                else if (strcmp(line, "\n") == 0)
+                {
+                }
                 else
                 {
-                    /*  printf("%s \n", line); */
-                    leer_comando(command, parameters, line);
-                    ejecutar_comando(command, parameters, line);
+                    //printf("zoooo %s \n", line);
+                    int posRedi = leer_comando(command, parameters, strdup(line));
+                    ejecutar_comando(command, parameters, strdup(line), posRedi);
+                    redirection = 0;
                 }
             }
+            fclose(fp);
         }
-        fclose(fp);
     }
     //Modo interactivo
     else if (argc == 1)
     {
         salidaGlobal = 0;
+        redirection = 0;
         while (salidaGlobal != 1)
         {
             type_prompt(); //mostrar pantalla prompt
             char *line = NULL;
             size_t size = 0;
             getline(&line, &size, stdin);
-            leer_comando(command, parameters, line); // leer el input
-            ejecutar_comando(command, parameters, line);
+            int posRedi = leer_comando(command, parameters, line); // leer el input
+            //printf("hay > ? %d en la posicion: %d \n" , redirection, posRedi);
+            ejecutar_comando(command, parameters, line, posRedi);
+            redirection = 0;
         }
     }
     else
